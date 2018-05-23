c4ea59d14611b1f985c32fabd231dfff7eece43c
Jan Pecka <jan.pecka@gmail.com>
2018-05-23 14:54:32 +0200

    Http request and xmlreader

--- MessageSender/Model/HealthCheck.cs
+++ MessageSender/Model/HealthCheck.cs
@@ -1,22 +1,36 @@
 ﻿using System.Net;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using System.Xml;
 
 namespace MessageSender.Model
 {
     class HealthCheck
     {
+
         public HealthStatus GetHealthStatus(string adress)
         {
+            string data = "";
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(adress);
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
             int statusCode = (int)response.StatusCode;
 
-            if(statusCode != 200)
+            if (statusCode != 200)
             {
-                return new HealthStatus() { ServerResponseStatus = statusCode};
-            }
+                return new HealthStatus() { ServerResponseStatus = statusCode };
+            };
+
+            Stream stream = response.GetResponseStream();
+            StreamReader streamReader = new StreamReader(stream);
+            data = streamReader.ReadToEnd();
+            XmlReader reader = XmlReader.Create(new StringReader(data));
+            
+            
+            
+            
+
         }
     }   
 }
--- MessageSender/Model/HealthStatus.cs
+++ MessageSender/Model/HealthStatus.cs
@@ -6,9 +6,9 @@ namespace MessageSender.Model
 {
     class HealthStatus
     {
-        public bool DbStatus;
+        public bool DbStatus = true;
         public string Version { get; set; }
         public int ServerResponseStatus { get; set; }
-        public List<WorkerStatus> workerList { get; set; }
+        public List<WorkerStatus> WorkerList { get; set; }
     }
 }
